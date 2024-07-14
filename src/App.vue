5f9a67a44c9a36c2d5811047892f9205d9438b99
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-07-14 15:27:40 +0000

    Modification du Header

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@ import Home from "./Pages/Home.vue";
 import Home2 from "./Pages/Home2.vue";
 import Products from "./Pages/Products.vue";
 import ShopingCart from "./Pages/ShopingCart.vue";
+import About from "./Pages/About.vue";
 </script>
 
 <template>
-  <Home2 />
+  <Products />
 </template>
 
 <style scoped>
