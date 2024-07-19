3021d78f19d923e47eab3e9ab70d543a1123e57f
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-07-19 19:02:48 +0000

    Gerer la page liste des producits

--- src/Pages/ProductDetail.vue
+++ src/Pages/ProductDetail.vue
@@ -138,8 +138,8 @@ const addToCartHandler = () => {
 
                 <div class="flex-w flex-r-m p-b-10">
                   <div class="size-204 flex-w flex-m respon6-next">
-                    <div class="wrap-num-product flex-w m-r-20 m-tb-10">
-                      <div @click="quantity > 1 ? quantity-- : 1"  class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+<!--                    <div class="wrap-num-product flex-w m-r-20 m-tb-10">
+                      <div @click="quantity > 1 ? quantity&#45;&#45; : 1"  class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
                         <i class="fs-16 zmdi zmdi-minus"></i>
                       </div>
 
@@ -148,9 +148,9 @@ const addToCartHandler = () => {
                       <div @click="quantity < product.stock ? quantity++ : 1" class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
                         <i class="fs-16 zmdi zmdi-plus"></i>
                       </div>
-                    </div>
+                    </div>-->
 
-                    <button class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail " @click="addToCartHandler">
+                    <button class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail mt-4" @click="addToCartHandler">
                       Ajouter au panier
                     </button>
                   </div>
